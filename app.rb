31a1b3bd602b5dac4007fd7972373376c030a45f
Cassio <cassiopgodinho@gmail.com>
2023-05-29 21:29:53 -0300

    split views in templates and add a js counter to make it clear the page is not fully reloading

--- app.rb
+++ app.rb
@@ -1,11 +1,31 @@
 require 'roda'
-
+require 'random/formatter'
 class App < Roda
+  $todos = [{ id: Random.uuid, name: 'First' }]
   plugin :render
+  plugin :type_routing, types: {
+    turbo: 'text/vnd.turbo-stream.html'
+  }
   route do |r|
     # GET / request
     r.root do
-      render('index')
+      view('index', locals: { todos: $todos })
+    end
+
+    r.post do
+      puts request.params
+      if request.params["todo"].empty?
+        todo = { id: Random.uuid, name: 'Second' }
+        $todos << todo
+      else
+        todo = { id: Random.uuid, name: request.params["todo"]}
+        $todos << todo
+      end
+
+      response.status = 301
+
+      r.turbo { render('todo_stream', locals: { todo: todo })}
+      r.html { view('index', locals: { todos: $todos }) }
     end
   end
 end
